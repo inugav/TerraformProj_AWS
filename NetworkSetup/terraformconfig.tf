e2283f2e8ecca5bca5a8d84d4e53f118b91f09ac
Vamsi Inuganti <vamsi_iv@hotmail.com>
2020-09-14 19:57:55 +0530

    terraform config Update

--- NetworkSetup/terraformconfig.tf
+++ NetworkSetup/terraformconfig.tf
@@ -19,7 +19,7 @@ terraform {
   tags   = { Project = "${var.project_tag}" }
 }*/
 
-resource "aws_dynamodb_table" "terraform_locks" {
+/*resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-up-and-running-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
@@ -27,4 +27,4 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
-}
+}*/
